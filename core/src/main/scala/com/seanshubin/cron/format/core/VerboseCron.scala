292a0abc281440e7599a3aff5ed1d46a6a4acf03
Sean Shubin <sshubin@cj.com>
2014-10-20 19:21:12 -0700

    (has failing tests) prototyping rewrite

--- core/src/main/scala/com/seanshubin/cron/format/core/VerboseCron.scala
+++ core/src/main/scala/com/seanshubin/cron/format/core/VerboseCron.scala
@@ -3,81 +3,276 @@ package com.seanshubin.cron.format.core
 import scala.collection.mutable.ArrayBuffer
 
 object VerboseCron {
+  def cronToVerboseCron(cron: String): Either[String, String] = {
+    val cronParts = cron.split("\\s+", -1).toSeq
+    val partsAndParsers = cronParts zip cronPartParsers
+    val builder = partsAndParsers.foldLeft(ParsedCronBuilder())(parseCronRange)
+    builder.build()
+  }
 
-  class Minute {
-    def parse(text: String): Either[String, String] = {
-      try {
-        val value = text.toInt
-        if (value < 0 || value > 59) Left(s"invalid value for minute: '$value' is not between 0 and 59")
-        else Right(s"minute is $value")
-      } catch {
-        case ex: NumberFormatException =>
-          Left(s"invalid value for minute: '$text' is not a whole number")
+  case class ParsedCronBuilder(parsedCronParts: List[String] = Nil, errors: List[String] = Nil) {
+    def withPartAndParser(partAndParser: (String, CronPartParser)): ParsedCronBuilder = {
+      val (part, parser) = partAndParser
+      parser.parseRange(part) match {
+        case Left(error) => copy(errors = error +: errors)
+        case Right(parsed) => copy(parsedCronParts = parsed +: parsedCronParts)
+      }
+    }
+
+    def build(): Either[String, String] = {
+      if (errors.isEmpty) {
+        Right(parsedCronParts.reverse.mkString(", "))
+      } else {
+        Left(errors.reverse.mkString(", "))
       }
     }
   }
 
-  class Hour {
-    def parse(text: String): Either[String, String] = {
-      try {
-        val value = text.toInt
-        if (value < 0 || value > 59) Left(s"invalid value for hour: '$value' is not between 0 and 59")
-        else Right(s"hour is $value")
-      } catch {
-        case ex: NumberFormatException =>
-          Left(s"invalid value for hour: '$text' is not a whole number")
+  def parseCronRange(builder: ParsedCronBuilder, partAndParser: (String, CronPartParser)): ParsedCronBuilder = {
+    builder.withPartAndParser(partAndParser)
+  }
+
+  trait CronRange {
+    def matches(text: String): Boolean
+
+    def parse(text: String, partParser: CronPartParser): Either[String, String]
+  }
+
+  object Every extends CronRange {
+    override def matches(text: String): Boolean = text == "*"
+
+    override def parse(text: String, partParser: CronPartParser): Either[String, String] = {
+      if (text == "*") Right(s"every ${partParser.singular}")
+      else Left(s"Expected '*', got '$text'")
+    }
+  }
+
+  object Between extends CronRange {
+    override def matches(text: String): Boolean = text.contains("-")
+
+    override def parse(text: String, partParser: CronPartParser): Either[String, String] = {
+      val parts = text.split("-", -1).toSeq
+      collapseEithers(parts.map(partParser.parse)) match {
+        case Right(values) =>
+          if (values.size == 2) Right(s"${partParser.singular} is between ${values(1)} and ${values(0)}")
+          else Left(s"Only 2 values allowed in a range, got ${values.size}")
+        case Left(errors) =>
+          Left(errors.mkString(", "))
       }
     }
   }
 
-  class DayOfMonth {
-    def parse(text: String): Either[String, String] = {
-      try {
-        val value = text.toInt
-        if (value < 1 || value > 31) Left(s"invalid value for day: '$value' is not between 1 and 31")
-        else Right(s"day is $value")
-      } catch {
-        case ex: NumberFormatException =>
-          Left(s"invalid value for day: '$text' is not a whole number")
+  def collapseEithers(eithers: Seq[Either[String, String]]): Either[Seq[String], Seq[String]] = {
+    val initialValue:Either[Seq[String], Seq[String]] = Right(Seq())
+    eithers.foldLeft(initialValue)(collapseEither)
+  }
+  
+  def collapseEither(soFar:Either[Seq[String], Seq[String]], current: Either[String, String]):Either[Seq[String], Seq[String]] = {
+    soFar match {
+      case Left(errors) =>
+        current match {
+          case Left(error) => Left(error +: errors)
+          case Right(value) => soFar
+        }
+      case Right(values)=>
+        current match {
+          case Left(error) => Left(Seq(error))
+          case Right(value) => Right(value +: values)
+        }
+    }
+  }
+
+  object Step extends CronRange {
+    override def matches(text: String): Boolean = text.contains("/")
+
+    override def parse(text: String, partParser: CronPartParser): Either[String, String] = {
+      val parts = text.split("/", -1).toSeq
+      collapseEithers(parts.map(partParser.parse)) match {
+        case Right(values) =>
+          if (values.size == 2) {
+            val start = values(1).toInt
+            val step = values(0).toInt
+            val value = if (start == 0) {
+              if (step == 1) s"every ${partParser.singular}"
+              else s"every $step ${partParser.plural}"
+            }
+            else {
+              if (step == 1) s"every ${partParser.singular} starting at $start"
+              else s"every $step ${partParser.plural} starting at $start"
+            }
+            Right(value)
+          } else Left(s"Only 2 values allowed in a range, got ${values.size}")
+        case Left(errors) =>
+          Left(errors.mkString(", "))
       }
     }
   }
 
-  class Month {
+  object Values extends CronRange {
+    override def matches(text: String): Boolean = text.contains(",")
+
+    override def parse(text: String, partParser: CronPartParser): Either[String, String] = ???
+  }
+
+  object Question extends CronRange {
+    override def matches(text: String): Boolean = text.contains("?")
+
+    override def parse(text: String, partParser: CronPartParser): Either[String, String] = ???
+  }
+
+  object Single extends CronRange {
+    override def matches(text: String): Boolean =
+      !text.contains("?") &&
+        !text.contains("*") &&
+        !text.contains("/") &&
+        !text.contains("-") &&
+        !text.contains(",")
+
+    override def parse(text: String, partParser: CronPartParser): Either[String, String] = {
+      partParser.parse(text) match {
+        case Left(error) => Left(error)
+        case Right(value) => Right(s"${partParser.singular} is $value")
+      }
+    }
+  }
+
+  val cronRanges: Seq[CronRange] = Seq(Every, Between, Step, Values, Question, Single)
+
+  trait CronPartParser {
+    def parse(text: String): Either[String, String]
+
+    def parseRange(text: String): Either[String, String] = {
+      val howManyMatching = cronRanges.count(_.matches(text))
+      val result = if (howManyMatching == 0) {
+        Left(s"'$text' does not match a cron range type")
+      } else if (howManyMatching == 1) {
+        val theRange = cronRanges.filter(_.matches(text)).head
+        theRange.parse(text, this)
+      } else {
+        Left(s"'$text' matches multiple cron range types, which is not currently supported")
+      }
+      result
+    }
+
+    def singular: String
+    def plural: String
+  }
+
+  class Second extends CronPartParser {
+    def parse(text: String): Either[String, String] = {
+      try {
+        val value = text.toInt
+        if (value < 0 || value > 59) Left(s"invalid value for $singular: '$value' is not between 0 and 59")
+        else Right(s"$value")
+      } catch {
+        case ex: NumberFormatException =>
+          Left(s"invalid value for $singular: '$text' is not a whole number")
+      }
+    }
+
+    override def singular: String = "second"
+
+    override def plural: String = "seconds"
+  }
+
+  class Minute extends CronPartParser {
+    def parse(text: String): Either[String, String] = {
+      try {
+        val value = text.toInt
+        if (value < 0 || value > 59) Left(s"invalid value for $singular: '$value' is not between 0 and 59")
+        else Right(s"$value")
+      } catch {
+        case ex: NumberFormatException =>
+          Left(s"invalid value for $singular: '$text' is not a whole number")
+      }
+    }
+
+    override def singular: String = "minute"
+
+    override def plural: String = "minutes"
+  }
+
+  class Hour extends CronPartParser {
+    def parse(text: String): Either[String, String] = {
+      try {
+        val value = text.toInt
+        if (value < 0 || value > 59) Left(s"invalid value for $singular: '$value' is not between 0 and 59")
+        else Right(s"$value")
+      } catch {
+        case ex: NumberFormatException =>
+          Left(s"invalid value for $singular: '$text' is not a whole number")
+      }
+    }
+
+    override def singular: String = "hour"
+
+    override def plural: String = "hours"
+  }
+
+  class DayOfMonth extends CronPartParser {
+    def parse(text: String): Either[String, String] = {
+      try {
+        val value = text.toInt
+        if (value < 1 || value > 31) Left(s"invalid value for $singular: '$value' is not between 1 and 31")
+        else Right(s"$value")
+      } catch {
+        case ex: NumberFormatException =>
+          Left(s"invalid value for $singular: '$text' is not a whole number")
+      }
+    }
+
+    override def singular: String = "day"
+
+    override def plural: String = "days"
+  }
+
+  class Month extends CronPartParser {
     def parse(text: String): Either[String, String] = {
       val maybeMonth = MonthEnum.fromString(text)
       maybeMonth match {
         case Some(monthEnum) =>
-          Right(s"month is ${monthEnum.name}")
+          Right(s"${monthEnum.name}")
         case None =>
-          Left(s"invalid value for month: '$text' is not a whole number between 1 and 12, and is not a 3-letter month abbreviation")
+          Left(s"invalid value for $singular: '$text' is not a whole number between 1 and 12, and is not a 3-letter $singular abbreviation")
       }
     }
+
+    override def singular: String = "month"
+
+    override def plural: String = "months"
   }
 
-  class DayOfWeek {
+  class DayOfWeek extends CronPartParser {
     def parse(text: String): Either[String, String] = {
       val maybeDayOfWeek = DayOfWeekEnum.fromString(text)
       maybeDayOfWeek match {
         case Some(dayOfWeekEnum) =>
-          Right(s"day of week is ${dayOfWeekEnum.name}")
+          Right(s"${dayOfWeekEnum.name}")
         case None =>
-          Left(s"invalid value for day of week: '$text' is not a whole number between 1 and 7, and is not a 3-letter day of week abbreviation")
+          Left(s"invalid value for day of $singular: '$text' is not a whole number between 1 and 7, and is not a 3-letter day of $singular abbreviation")
       }
     }
+
+    override def singular: String = "week"
+
+    override def plural: String = "weeks"
   }
 
-  class Year {
+  class Year extends CronPartParser {
     def parse(text: String): Either[String, String] = {
       try {
         val value = text.toInt
-        if (value < 1900 || value > 3000) Left(s"invalid value for year: '$value' is not between 1900 and 3000")
-        else Right(s"year is $value")
+        if (value < 1900 || value > 3000) Left(s"invalid value for $singular: '$value' is not between 1900 and 3000")
+        else Right(s"$value")
       } catch {
         case ex: NumberFormatException =>
-          Left(s"invalid value for year: '$text' is not a whole number")
+          Left(s"invalid value for $singular: '$text' is not a whole number")
       }
     }
+
+    override def singular: String = "year"
+
+    override def plural: String = "years"
   }
 
   sealed abstract case class MonthEnum(ordinal: Int, name: String, shortName: String) {
@@ -139,10 +334,13 @@ object VerboseCron {
     }
   }
 
+  val second: Second = new Second
   val minute: Minute = new Minute
   val hour: Hour = new Hour
   val dayOfMonth: DayOfMonth = new DayOfMonth
   val month: Month = new Month
   val dayOfWeek: DayOfWeek = new DayOfWeek
   val year: Year = new Year
+
+  val cronPartParsers: Seq[CronPartParser] = Seq(second, minute, hour, dayOfMonth, month, dayOfWeek, year)
 }
--- core/src/test/scala/com/seanshubin/cron/format/core/VerboseCronTest.scala
+++ core/src/test/scala/com/seanshubin/cron/format/core/VerboseCronTest.scala
@@ -3,6 +3,29 @@ package com.seanshubin.cron.format.core
 import org.scalatest.FunSuite
 
 class VerboseCronTest  extends FunSuite {
+  test("second") {
+    def valid(input:String, expected:String) = {
+      VerboseCron.second.parse(input) match {
+        case Left(message) =>
+          fail(s"expected valid value '$expected', but got error '$message'")
+        case Right(actual) =>
+          assert(actual === expected)
+      }
+    }
+    def invalid(input:String, expected:String) = {
+      VerboseCron.second.parse(input) match {
+        case Left(message) =>
+          assert(message === expected)
+        case Right(actual) =>
+          fail(s"expected error message '$expected', but got valid value '$actual'")
+      }
+    }
+    valid("0", "0")
+    valid("59", "59")
+    invalid("-1", "invalid value for second: '-1' is not between 0 and 59")
+    invalid("60", "invalid value for second: '60' is not between 0 and 59")
+    invalid("nan", "invalid value for second: 'nan' is not a whole number")
+  }
   test("minute") {
     def valid(input:String, expected:String) = {
       VerboseCron.minute.parse(input) match {
@@ -20,8 +43,8 @@ class VerboseCronTest  extends FunSuite {
           fail(s"expected error message '$expected', but got valid value '$actual'")
       }
     }
-    valid("0", "minute is 0")
-    valid("59", "minute is 59")
+    valid("0", "0")
+    valid("59", "59")
     invalid("-1", "invalid value for minute: '-1' is not between 0 and 59")
     invalid("60", "invalid value for minute: '60' is not between 0 and 59")
     invalid("nan", "invalid value for minute: 'nan' is not a whole number")
@@ -43,8 +66,8 @@ class VerboseCronTest  extends FunSuite {
           fail(s"expected error message '$expected', but got valid value '$actual'")
       }
     }
-    valid("0", "hour is 0")
-    valid("59", "hour is 59")
+    valid("0", "0")
+    valid("59", "59")
     invalid("-1", "invalid value for hour: '-1' is not between 0 and 59")
     invalid("60", "invalid value for hour: '60' is not between 0 and 59")
     invalid("nan", "invalid value for hour: 'nan' is not a whole number")
@@ -66,8 +89,8 @@ class VerboseCronTest  extends FunSuite {
           fail(s"expected error message '$expected', but got valid value '$actual'")
       }
     }
-    valid("1", "day is 1")
-    valid("31", "day is 31")
+    valid("1", "1")
+    valid("31", "31")
     invalid("0", "invalid value for day: '0' is not between 1 and 31")
     invalid("32", "invalid value for day: '32' is not between 1 and 31")
     invalid("nan", "invalid value for day: 'nan' is not a whole number")
@@ -89,8 +112,8 @@ class VerboseCronTest  extends FunSuite {
           fail(s"expected error message '$expected', but got valid value '$actual'")
       }
     }
-    valid("1", "month is January")
-    valid("12", "month is December")
+    valid("1", "January")
+    valid("12", "December")
     invalid("0", "invalid value for month: '0' is not a whole number between 1 and 12, and is not a 3-letter month abbreviation")
     invalid("13", "invalid value for month: '13' is not a whole number between 1 and 12, and is not a 3-letter month abbreviation")
     invalid("nan", "invalid value for month: 'nan' is not a whole number between 1 and 12, and is not a 3-letter month abbreviation")
@@ -112,30 +135,30 @@ class VerboseCronTest  extends FunSuite {
           fail(s"expected error message '$expected', but got valid value '$actual'")
       }
     }
-    valid("jan", "month is January")
-    valid("feb", "month is February")
-    valid("mar", "month is March")
-    valid("apr", "month is April")
-    valid("may", "month is May")
-    valid("jun", "month is June")
-    valid("jul", "month is July")
-    valid("aug", "month is August")
-    valid("sep", "month is September")
-    valid("oct", "month is October")
-    valid("nov", "month is November")
-    valid("dec", "month is December")
-    valid("JAN", "month is January")
-    valid("FEB", "month is February")
-    valid("MAR", "month is March")
-    valid("APR", "month is April")
-    valid("MAY", "month is May")
-    valid("JUN", "month is June")
-    valid("JUL", "month is July")
-    valid("AUG", "month is August")
-    valid("SEP", "month is September")
-    valid("OCT", "month is October")
-    valid("NOV", "month is November")
-    valid("DEC", "month is December")
+    valid("jan", "January")
+    valid("feb", "February")
+    valid("mar", "March")
+    valid("apr", "April")
+    valid("may", "May")
+    valid("jun", "June")
+    valid("jul", "July")
+    valid("aug", "August")
+    valid("sep", "September")
+    valid("oct", "October")
+    valid("nov", "November")
+    valid("dec", "December")
+    valid("JAN", "January")
+    valid("FEB", "February")
+    valid("MAR", "March")
+    valid("APR", "April")
+    valid("MAY", "May")
+    valid("JUN", "June")
+    valid("JUL", "July")
+    valid("AUG", "August")
+    valid("SEP", "September")
+    valid("OCT", "October")
+    valid("NOV", "November")
+    valid("DEC", "December")
   }
   test("day of week - numeric") {
     def valid(input:String, expected:String) = {
@@ -154,8 +177,8 @@ class VerboseCronTest  extends FunSuite {
           fail(s"expected error message '$expected', but got valid value '$actual'")
       }
     }
-    valid("1", "day of week is Sunday")
-    valid("7", "day of week is Saturday")
+    valid("1", "Sunday")
+    valid("7", "Saturday")
     invalid("0", "invalid value for day of week: '0' is not a whole number between 1 and 7, and is not a 3-letter day of week abbreviation")
     invalid("8", "invalid value for day of week: '8' is not a whole number between 1 and 7, and is not a 3-letter day of week abbreviation")
     invalid("nan", "invalid value for day of week: 'nan' is not a whole number between 1 and 7, and is not a 3-letter day of week abbreviation")
@@ -177,20 +200,20 @@ class VerboseCronTest  extends FunSuite {
           fail(s"expected error message '$expected', but got valid value '$actual'")
       }
     }
-    valid("sun", "day of week is Sunday")
-    valid("mon", "day of week is Monday")
-    valid("tue", "day of week is Tuesday")
-    valid("wed", "day of week is Wednesday")
-    valid("thu", "day of week is Thursday")
-    valid("fri", "day of week is Friday")
-    valid("sat", "day of week is Saturday")
-    valid("SUN", "day of week is Sunday")
-    valid("MON", "day of week is Monday")
-    valid("TUE", "day of week is Tuesday")
-    valid("WED", "day of week is Wednesday")
-    valid("THU", "day of week is Thursday")
-    valid("FRI", "day of week is Friday")
-    valid("SAT", "day of week is Saturday")
+    valid("sun", "Sunday")
+    valid("mon", "Monday")
+    valid("tue", "Tuesday")
+    valid("wed", "Wednesday")
+    valid("thu", "Thursday")
+    valid("fri", "Friday")
+    valid("sat", "Saturday")
+    valid("SUN", "Sunday")
+    valid("MON", "Monday")
+    valid("TUE", "Tuesday")
+    valid("WED", "Wednesday")
+    valid("THU", "Thursday")
+    valid("FRI", "Friday")
+    valid("SAT", "Saturday")
   }
   test("year") {
     def valid(input:String, expected:String) = {
@@ -209,10 +232,71 @@ class VerboseCronTest  extends FunSuite {
           fail(s"expected error message '$expected', but got valid value '$actual'")
       }
     }
-    valid("1900", "year is 1900")
-    valid("3000", "year is 3000")
+    valid("1900", "1900")
+    valid("3000", "3000")
     invalid("1899", "invalid value for year: '1899' is not between 1900 and 3000")
     invalid("3001", "invalid value for year: '3001' is not between 1900 and 3000")
     invalid("nan", "invalid value for year: 'nan' is not a whole number")
   }
+  test("cron mini pattern") {
+    val testData = Seq(
+      ("*", "every foo"),
+      ("5", "foo is 5"),
+      ("1/4", "every 4 foos starting at 1"),
+      ("5-10", "foo is between 5 and 10"),
+      ("11,15,27", "foo is 11 15 27"))
+    val fakeCronPartParser = new VerboseCron.CronPartParser {
+      override def parse(text: String): Either[String, String] = {
+        try {
+          Right(s"$text")
+        } catch {
+          case ex:NumberFormatException => Left(s"unable to convert '$text' to a number")
+        }
+      }
+
+      override def singular: String = "foo"
+
+      override def plural: String = "foos"
+    }
+    def verify(testDatum: (String, String)) = {
+      val (cronElement, expected) = testDatum
+      val actual = fakeCronPartParser.parseRange(cronElement)
+      assert(actual === Right(expected))
+    }
+    testData.foreach(verify)
+  }
+  test("describe cron") {
+    val testData = Seq(
+      ("1 2 3 4 5 6 1978", "second is 1, minute is 2, hour is 3, day is 4, month is May, day of week is Friday, year is 1978"),
+      ("1 2 3 4 may fri 1978", "second is 1, minute is 2, hour is 3, day is 4, month is May, day of week is Friday, year is 1978")
+//      ,
+//      ("0 0 0 * * ? *", "every day at midnight"),
+//      ("1 0 0 * * ? *", "second is 1"),
+//      ("2 0 0 * * ? *", "second is 2"),
+//      ("2-4 0 0 * * ? *", "second is between 2 and 4"),
+//      ("1/5 0 0 * * ? *", "every 5 seconds starting at 1"),
+//      ("5/1 0 0 * * ? *", "every second starting at 5"),
+//      ("1,2,3 0 0 * * ? *", "second is 1 2 3"),
+//      ("* 0 0 * * ? *", "every second"),
+//      ("0 3-5 0 * * ? *", "minute is between 3 and 5"),
+//      ("0 0 3/1 * * ? *", "every hour starting at 3"),
+//      ("0 0 1/3 * * ? *", "every 3 hours starting at 1"),
+//      ("0 0 0/3 * * ? *", "every 3 hours"),
+//      ("0 0 0 1 * ? *", "day is 1"),
+//      ("0 0 0 * 2 ? *", "month is 2"),
+//      ("0 0 0 * * ? 2000", "year is 2000"),
+//      ("12 0 23 * 11 ? 1978", "second is 12, hour is 23, month is 11, year is 1978"),
+//      ("0 23 0 15 * ? *", "minute is 23, day is 15"),
+//      ("45 23 8 15 5 ? 2001", "second is 45, minute is 23, hour is 8, day is 15, month is 5, year is 2001"),
+//      ("30-45 2/3 * 2,3,7 5 ? 2001-2005", "second is between 30 and 45, every 3 minutes starting at 2, every hour, day is 2 3 7, month is 5, year is between 2001 and 2005"),
+//      ("0 0 * * * ? *", "every hour")
+    )
+    def verify(testTuple:(String, String)): Unit = {
+      val (cron, expected) = testTuple
+      val actual = VerboseCron.cronToVerboseCron(cron)
+      assert(actual === Right(expected))
+    }
+    testData.foreach(verify)
+  }
+
 }
